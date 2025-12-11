e28b748f4a727b0a5a2c4f571ec7f7dc6bb9f098
AgustinMGauto <agustin_gauto@outlook.com>
2025-12-11 21:12:36 +0000

    correccion de errores

--- test.js
+++ test.js
@@ -9,16 +9,16 @@ test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromEuroToDollar(3.5)).toBeCloseTo(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
 test("un dolar deberia ser 146.26 yenes", function() {
     const { fromDollarToYen } = require('./app.js');
 
     const dolayen = fromDollarToYen(5);
-    const expected = 5 * 146.26;
+    const expected = 5 * 146.261682;
 
-    expect(dolayen).toBe(expected); 
+    expect(dolayen).toBeCloseTo(expected); 
 })
 
 test("un yen deberia ser 0.00556 libras esterlinas", function() {
@@ -27,5 +27,5 @@ test("un yen deberia ser 0.00556 libras esterlinas", function() {
     const yenalibras = fromYenToPound(15);
     const expected = 15 * 0.00556;
 
-    expect(yenalibras).toBe(expected); 
+    expect(yenalibras).toBeCloseTo(expected); 
 })
